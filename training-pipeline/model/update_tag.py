cd78e26b25beb3e644fa9a81043f6fa1d4ef27a2
ShaoXiangChien <ericchien21@gapp.nthu.edu.tw>
2022-12-08 10:20:31 +0800

    update model name

--- training-pipeline/model/update_tag.py
+++ training-pipeline/model/update_tag.py
@@ -6,7 +6,7 @@ from datetime import datetime as dt
 def upload_prod_model():
     ws = Workspace.from_config("./model")
     now = dt.now()
-    ws.models['diabetes-model'].download("./", exist_ok=True)
+    ws.models['diabetes_model'].download("./", exist_ok=True)
     model = Model.register(model_path="model",
                             model_name="diabetes_model",
                             tags={'tags': "prod", "date": now.strftime("%Y-%m-%d")},
